2393e6bc5fffb31c92d60f9c3aa9848a1902589a
Berivan Kürekci <kuerekci@puzzle.ch>
2021-06-04 16:28:02 +0200

    Refactor semester, yearcalenderweek models and yaml

--- src/main/java/model/Semester.java
+++ src/main/java/model/Semester.java
@@ -1,12 +1,22 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Semester{
-    private int id;
-    private YearCalendarWeek[] yearCalendarWeeks;
+    private ArrayList<YearCalendarWeek> year_calendar_weeks;
 
+    public Semester() {
+    }
 
-    public Semester(int id, YearCalendarWeek[] yearCalendarWeek) {
-        this.id = id;
-        this.yearCalendarWeeks = yearCalendarWeek;
+    public Semester(ArrayList<YearCalendarWeek> year_calendar_weeks) {
+        this.year_calendar_weeks = year_calendar_weeks;
+    }
+
+    public ArrayList<YearCalendarWeek> getYear_calendar_weeks() {
+        return year_calendar_weeks;
+    }
+
+    public void setYear_calendar_weeks(ArrayList<YearCalendarWeek> year_calendar_weeks) {
+        this.year_calendar_weeks = year_calendar_weeks;
     }
 }
--- src/main/java/model/YearCalendarWeek.java
+++ src/main/java/model/YearCalendarWeek.java
@@ -1,15 +1,31 @@
 package model;
 
 public class YearCalendarWeek {
-    private int id;
-    private String level;
-    private int apprenticeshipYear;
-    private int startWeek;
-    private int endWeek;
+    private int start_at_week_nr;
+    private int finish_at_week_nr;
 
-    public YearCalendarWeek(int id, int startWeek, int endWeek) {
-        this.id = id;
-        this.startWeek = startWeek;
-        this.endWeek = endWeek;
+
+    public YearCalendarWeek() {
+    }
+
+    public YearCalendarWeek(int start_at_week_nr, int finish_at_week_nr) {
+        this.start_at_week_nr = start_at_week_nr;
+        this.finish_at_week_nr = finish_at_week_nr;
+    }
+
+    public int getStart_at_week_nr() {
+        return start_at_week_nr;
+    }
+
+    public void setStart_at_week_nr(int start_at_week_nr) {
+        this.start_at_week_nr = start_at_week_nr;
+    }
+
+    public int getFinish_at_week_nr() {
+        return finish_at_week_nr;
+    }
+
+    public void setFinish_at_week_nr(int finish_at_week_nr) {
+        this.finish_at_week_nr = finish_at_week_nr;
     }
 }
